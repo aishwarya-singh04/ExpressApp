20dc905f7392334d23ac382d3f447fd6e8ad3d77
aishwarya-singh04 <aishwarya.singh_cs18@gla.ac.in>
2021-04-14 13:08:14 +0530

    commit

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const express=require("express");
 
 // 2. Initialize your express app
 const app= express();
+
+//Generalize production port
+const PORT= process.env.PORT || 4000;           //available port will be provided
+
+
+//3. Listen server request on PORT
+app.listen(PORT, function(){
+    console.log(`Server started at PORT ${PORT}`);
+});
