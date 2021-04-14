f2323df23e98ea20b70d2a457384b396f1712205
aishwarya-singh04 <aishwarya.singh_cs18@gla.ac.in>
2021-04-14 13:36:14 +0530

    commit

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 // 1. Require your Express model
-const express=require("express");
+const express= require("express");
 
 // 2. Initialize your express app
 const app= express();
 
 //Generalize production port
-const PORT= process.env.PORT || 4000;           //available port will be provided
+const PORT= process.env.PORT || 5000;           //available port will be provided
 
 
 //3. Listen server request on PORT
 app.listen(PORT, function(){
     console.log(`Server started at PORT ${PORT}`);
 });
+
+
+//Handle Client Request (get, post, put, delete)
+// app.method('Route', callback(){})
+
+//Handle '/' Home Route
+app.get('/', function(req, res){
+    //making client-server model
+    res.send("Hello from Express Server");
+});
+
+app.get('/about', function(req, res){
+    res.send("Welcome to About Page");
+})
